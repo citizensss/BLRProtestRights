88f79934907d0a720195037049d9d61638d701c1
happypplka <132691126+happypplka@users.noreply.github.com>
2023-05-06 03:28:00 +0530

    Update tweets.js

--- tweets.js
+++ tweets.js
@@ -33,6 +33,63 @@ const tweets = [
   "The current Karnataka govt has made no progress on Climate Change commitments, ignored Environmental Safeguards and has  left Pollution Largely Unchecked #BJPFailsKarnataka #VoteWisely #ಕರುನಾಡದ್ರೋಹಿಬಿಜೆಪಿ #KarnatakaAssemblyElections",
   "This government has failed to curb undemocratic activities by anti-social elements which affected harmony. #VoteWisely #BJPFailsKarnataka #ಕರುನಾಡದ್ರೋಹಿಬಿಜೆಪಿ #KarnatakaAssemblyElections",
   "This govt has failed to curb calls for economic boycott of Muslim vendors, harassing of Muslim girl students, and attack on Christians. This has affected the harmony in Karnataka. #VoteWisely #BJPFailsKarnataka #ಕರುನಾಡದ್ರೋಹಿಬಿಜೆಪಿ #KarnatakaAssemblyElections"
+  "The Karnataka government started the Vinay Samarasya yojane to eradicate untouchability but more than a year after it started it’s yet to be inaugurated or implemented #VoteWisely #BJPFailsKarnataka #ಕರುನಾಡದ್ರೋಹಿಬಿಜೆಪಿ #KarnatakaAssemblyElections",
+  "Since Sep 2020 Bengaluru has no elected municipal council. Elections have been delayed and Bengaluru has suffered. #VoteWisely #BJPFailsKarnataka #ಕರುನಾಡದ್ರೋಹಿಬಿಜೆಪಿ #KarnatakaAssemblyElections", 
+  "ತೊಲಗಿಸಿ ಭ್ರಷ್ಟ ಸರಕಾರ, ಯೋಚಿಸಿ ಮತ ಚಲಾಯಿಸಿ. #BJPFailsKarnataka #ಕರುನಾಡದ್ರೋಹಿಬಿಜೆಪಿ",
+  "ತೊಲಗಿಸಿ ಭ್ರಷ್ಟ ಸರಕಾರ, ಯೋಚಿಸಿ ಮತ ಚಲಾಯಿಸಿ. #BJPFailsKarnataka #ಕರುನಾಡದ್ರೋಹಿಬಿಜೆಪಿ",  
+  "FACTS: Over the past four years, Karnataka has been subject to more slogans & publicity than to actual substantial policies and programs. #VoteWisely #BJPFailsKarnataka #ಕರುನಾಡದ್ರೋಹಿಬಿಜೆಪಿ #KarnatakaAssemblyElections", 
+  "CM Bommai declared the ‘Panch Sutra’ for development policies. Yet, no specific policies have sought to address the many problems that disadvantaged regions & communities face. #VoteWisely #BJPFailsKarnataka #ಕರುನಾಡದ್ರೋಹಿಬಿಜೆಪಿ #KarnatakaAssemblyElections",
+  "In tandem with promoting dominant Caste Corporations, the Karnataka govt. has undertaken aggressive ‘statue construction’ & theme park development as a way of forging links with diverse caste groups. #VoteWisely #BJPFailsKarnataka #ಕರುನಾಡದ್ರೋಹಿಬಿಜೆಪಿ #KarnatakaAssemblyElections",
+  "Karnataka State Contractors’ Association levelled charges that this government was demanding 40% commissions for government contracts & clearance of bills. #VoteWisely #BJPFailsKarnataka #ಕರುನಾಡದ್ರೋಹಿಬಿಜೆಪಿ #KarnatakaAssemblyElections",
+  "Students across Karnataka are protesting the delay and non-payment of scholarships for SC, ST, BC, and OBC students. Why has the BJP goverenment stopped scholarships? #VoteWisely #BJPFailsKarnataka #KarnatakaAssemblyElections",
+  "Scholarships for Beedi workers’ children and Maulana Azad National Fellowships have been stopped by the Karnataka govt. affecting the education of many deserving students. #VoteWisely #BJPFailsKarnataka #ಕರುನಾಡದ್ರೋಹಿಬಿಜೆಪಿ #KarnatakaAssemblyElections",
+  "One contractor who committed suicide due to pressure from BJP's 40% commissions from contractors, actually named Minister Eshwarappa in his suicide note. #VoteWisely #BJPFailsKarnataka #ಕರುನಾಡದ್ರೋಹಿಬಿಜೆಪಿ #KarnatakaAssemblyElections", 
+  "In Karnataka under the ruling party, laws of criminal procedure were violated during arrests. #VoteWisely #BJPFailsKarnataka #ಕರುನಾಡದ್ರೋಹಿಬಿಜೆಪಿ #KarnatakaAssemblyElections", 
+  "In Karnataka, families were not informed why they were arrested or where they were taken, minors who were to be in juvenile facility were assigned to central jail.  #VoteWisely #BJPFailsKarnataka #ಕರುನಾಡದ್ರೋಹಿಬಿಜೆಪಿ #KarnatakaAssemblyElections",
+  "Nalin Kumar Kateel, President, BJP (Karnataka): “Focus on love jihaad, not issues such as roads and drains” (reported January 03, 2023).... #BJPFailsKarnataka  #VoteWisely #ಕರುನಾಡದ್ರೋಹಿಬಿಜೆಪಿ #KarnatakaAssemblyElections",
+  "Karnataka today: Reckless expansion of metros, destruction of coastal Karnataka, and a severely underfunded forest department. What are we leaving for our future generations? #BJPFailsKarnataka #VoteWisely #ಕರುನಾಡದ್ರೋಹಿಬಿಜೆಪಿ #KarnatakaAssemblyElections", 
+  "BJP MLA Madal Virupakshappa and his son, Prashant, also a KA state government official, were recently arrested on corruption charges after large unaccounted sums were found in their possession. #VoteWisely #BJPFailsKarnataka #ಕರುನಾಡದ್ರೋಹಿಬಿಜೆಪಿ #KarnatakaAssemblyElections", 
+  "The Land Reforms Amendment of 2020 affects labourers adversely, and is also the death-knell of small and marginal farmers. #BJPFailsKarnataka #VoteWisely #ಕರುನಾಡದ್ರೋಹಿಬಿಜೆಪಿ #KarnatakaAssemblyElections", 
+  "Attn voters ,don't forget. The Bommai govt *closed* 71 of the 194 Santwana Kendras in Karnataka, centres which support survivors of domestic and sexual violence.  #BJPFailsKarnataka #VoteWisely #ಕರುನಾಡದ್ರೋಹಿಬಿಜೆಪಿ #KarnatakaAssemblyElections",
+  "Karnataka BJP govt's hijab ban has violated multiple rights (to education,freedom, dignity, freedom of expression, right to non-discrimination & freedom from arbitrary state action) of Muslim girls. #VoteWisely #BJPFailsKarnataka #ಕರುನಾಡದ್ರೋಹಿಬಿಜೆಪಿ #KarnatakaAssemblyElections", 
+  "The govt has filed cases against pourakarmikas for protesting the violation of their rights, & trade unions for carrying out May day rallies. This government has reduced the space for dissent #VoteWisely #BJPFailsKarnataka #ಕರುನಾಡದ್ರೋಹಿಬಿಜೆಪಿ #KarnatakaAssemblyElections", 
+  "Teachers & Police sub-inspectors recruitment has now been exposed as a scam where heavy payments were made for being selected. While several people were arrested, the big fish went scot free. #VoteWisely #BJPFailsKarnataka #ಕರುನಾಡದ್ರೋಹಿಬಿಜೆಪಿ #KarnatakaAssemblyElections",
+  "Anyone who received as many fails as the current government of Karnataka would feel truly ashamed. #BJPFailsKarnataka #VoteWisely #ಕರುನಾಡದ್ರೋಹಿಬಿಜೆಪಿ  #KarnatakaAssemblyElections", 
+  "Karnataka Government has failed to protect state from perils of centralisation. #BJPFailsKarnataka #VoteWisely #ಕರುನಾಡದ್ರೋಹಿಬಿಜೆಪಿ #KarnatakaAssemblyElections", 
+  "The Karnataka State Government has ceded all decision-making power to New Delhi, leaving it unable to advocate for its own people. #BJPFailsKarnataka #VoteWisely #ಕರುನಾಡದ್ರೋಹಿಬಿಜೆಪಿ #KarnatakaAssemblyElections",
+  "NGO Chilume Trust was found to be engaged in voter data theft in connivance with State officials, while its sister concern Chilume Enterprises engaged in consulting for political parties #BJPFailsKarnataka  #VoteWisely #ಕರುನಾಡದ್ರೋಹಿಬಿಜೆಪಿ #KarnatakaAssemblyElections", 
+  "The Karnataka State Government has failed to advocate for Karnataka with the Central Government. For example, it took three months and much persuasion for the Centre to release funds for flood relief in 2021. #BJPFailsKarnataka #VoteWisely #ಕರುನಾಡದ್ರೋಹಿಬಿಜೆಪಿ ",
+  "Karnataka’s economic progress and resources are being expropriated. Our State’s unique cultural heritage is being eroded with the imposition of Hindi. #BJPFailsKarnataka #VoteWisely #ಕರುನಾಡದ್ರೋಹಿಬಿಜೆಪಿ #KarnatakaAssemblyElections",
+  "Several BJP legislators, beginning with the Chief Minister, have incited hate & violence in their unconstitutional remarks & public speeches. Yet they have gone unpunished. #VoteWisely #BJPFailsKarnataka #ಕರುನಾಡದ್ರೋಹಿಬಿಜೆಪಿ #KarnatakaAssemblyElections", 
+  "Karnataka State government has failed to protect its decision-making power on matters in the State Subjects list like  education, cooperatives, etc #BJPFailsKarnataka #VoteWisely #ಕರುನಾಡದ್ರೋಹಿಬಿಜೆಪಿ #KarnatakaAssemblyElections",
+  "The policies of the Karnataka government have led to the weakening of premier colleges like UVCE ..  #BJPFailsKarnataka #VoteWisely #ಕರುನಾಡದ್ರೋಹಿಬಿಜೆಪಿ #KarnatakaAssemblyElections",
+  "While condoning violent activities by Hindu vigilantes, CM Bommai said ‘Action & reaction are bound to happen when there is no morality in society'. This is unacceptable. #VoteWisely #BJPFailsKarnataka #ಕರುನಾಡದ್ರೋಹಿಬಿಜೆಪಿ #KarnatakaAssemblyElections",
+  "The KA govt's attempt to promote veg or ‘sattvic’ food in schools, despite criticism from civil society, nutritionists, the food commission, etc., goes against the interest of disadvantaged children. #VoteWisely #BJPFailsKarnataka #ಕರುನಾಡದ್ರೋಹಿಬಿಜೆಪಿ #KarnatakaAssemblyElections", 
+  "The sudden hijab ban by Karnataka's BJP government in govt. Pre-university Colleges has led to communalisation of educational institutes and caused violence across the state. #VoteWisely #BJPFailsKarnataka #ಕರುನಾಡದ್ರೋಹಿಬಿಜೆಪಿ #KarnatakaAssemblyElections", 
+  "The Karnataka State Contractors Association accused the Bommai-led government, legislators & officials of harassing them to pay a commission of up to 40 per cent on government tenders #BJPFailsKarnataka #VoteWisely #ಕರುನಾಡದ್ರೋಹಿಬಿಜೆಪಿ #KarnatakaAssemblyElections  
+  "Under the current leadership in Karnataka, Hijab-wearing Muslim women students were forced to choose between continuing their education and wearing the hijab. The girls of Karnataka deserve better. #BJPFailsKarnataka #VoteWisely #ಕರುನಾಡದ್ರೋಹಿಬಿಜೆಪಿ #KarnatakaAssemblyElections", 
+  "The unconstitutional Karnataka Protection of Right to Freedom of Religion Act blatantly infantilizes adult women by assuming that all adult women in interfaith relationships/marriages are gullible and are therefore, victims of “forced conversions”.#BJPFailsKarnataka #VoteWisely",
+  "ಜಿಎಸ್‌ಟಿ ಬಾಕಿ ಕೇಳಿದಾಗ ಕೊಡಲಿಲ್ಲ, ನೆರೆ ಬಂದಾಗ ಬರಲಿಲ್ಲ, ಚಾಮರಾಜನಗರದಲ್ಲಿ ೩೬ ಸತ್ತಾಗ ಬರಲಿಲ್ಲ. ಚುನಾವಣೆಯಲ್ಲಿ ಸೋಲುವ ಭೀತಿ ಬಂದಾಗ ಮಾತ್ರ ಬರುವ ಗುಮ್ಮ ಯಾರು? #BJPFailsKarnataka #ಕರುನಾಡದ್ರೋಹಿಬಿಜೆಪಿ", 
+  "Look at the trees damaged for weekend visit of Modi sir. He will come and go, but what about poor Bengalureans who have to suffer the harsh sun? ... #BJPFailsKarnataka #VoteWisely #ಕರುನಾಡದ್ರೋಹಿಬಿಜೆಪಿ #KarnatakaAssemblyElections",
+  "Karnataka needs a 'health resurrection'. Health issues have been relegated to the background as people across the country are yet to fully recover from the blows of the coronavirus pandemic... #BJPFailsKarnataka #VoteWisely #ಕರುನಾಡದ್ರೋಹಿಬಿಜೆಪಿ #KarnatakaAssemblyElections",
+  "Even though Karnataka has a high amount of landlessness, the government has failed to ensure Bagar Hukum cultivators are allotted the lands they cultivate. #BJPFailsKarnataka #VoteWisely #ಕರುನಾಡದ್ರೋಹಿಬಿಜೆಪಿ #KarnatakaAssemblyElections",
+  "ಇವತ್ತು ಲಿಂಗಾಯಿತರ ಅವಶ್ಯಕತೆ ಇಲ್ಲ, ನಾಳೆಯ ರೋಡ್ ಶೊ ನಂತರ ಕನ್ನಡಿಗರ ಅವಶ್ಯಕತೆಯೂ ಇರುವುದಿಲ್ಲ. #BJPFailsKarnataka #ಕರುನಾಡದ್ರೋಹಿಬಿಜೆಪಿ", 
+  "Lingayat seer Dingaleshwara Swamiji of the Balehosur Mutt accused officials of the Karnataka government of demanding a 30 per cent commission from mutts to release funds. #BJPFailsKarnataka #VoteWisely #ಕರುನಾಡದ್ರೋಹಿಬಿಜೆಪಿ #KarnatakaAssemblyElections", 
+  "Karnataka govt gave permission & land to a mega cultural-religious project of the Isha Foundation, where the Arkavathy River watershed is being recklessly urbanised #BJPFailsKarnataka #VoteWisely #ಕರುನಾಡದ್ರೋಹಿಬಿಜೆಪಿ #KarnatakaAssemblyElections",
+  
+"
+
+"
+"
+"", 
+"
+
+
+
+  
+  
+  
+
   
 
 
