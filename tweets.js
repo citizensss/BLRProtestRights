0cc2c41c704f2ac73985640a94da1c1bc2f2f93d
happypplka <132691126+happypplka@users.noreply.github.com>
2023-05-05 22:16:41 +0530

    Update tweets.js

--- tweets.js
+++ tweets.js
@@ -1,4 +1,9 @@
 const tweets = [
+  
+  
 "Karnataka has highest teacher vacancy in India. #VoteWisely #BJPFailsKarnataka #ಕರುನಾಡದ್ರೋಹಿಬಿಜೆಪಿ #KarnatakaAssemblyElections",
-"In Karnataka, the BJP has denied people's participation in governance by delaying BBMP, Zilla Panchayat, Taluk Elections. #VoteWisely. #BJPFailsKarnataka #ಕರುನಾಡದ್ರೋಹಿಬಿಜೆಪಿ #KarnatakaAssemblyElections "
-"BJP violated the people's mandate, and came to power by bringing down a democratically formed government & undermining democracy through Operation Kamala. #VoteWisely. #BJPFailsKarnataka #ಕರುನಾಡದ್ರೋಹಿಬಿಜೆಪಿ #KarnatakaAssemblyElections" 
+"In Karnataka, the BJP has denied people's participation in governance by delaying BBMP, Zilla Panchayat, Taluk Elections. #VoteWisely. #BJPFailsKarnataka #ಕರುನಾಡದ್ರೋಹಿಬಿಜೆಪಿ #KarnatakaAssemblyElections ",
+"BJP violated the people's mandate, and came to power by bringing down a democratically formed government & undermining democracy through Operation Kamala. #VoteWisely. #BJPFailsKarnataka #ಕರುನಾಡದ್ರೋಹಿಬಿಜೆಪಿ #KarnatakaAssemblyElections",
+
+  
+]
