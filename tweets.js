3739a505f2fe25097a42b02ce402c050a1c98d18
happypplka <132691126+happypplka@users.noreply.github.com>
2023-05-06 03:17:05 +0530

    Update tweets.js

--- tweets.js
+++ tweets.js
@@ -1,26 +1,39 @@
 const tweets = [
   
   
-"Karnataka has highest teacher vacancy in India. #VoteWisely #BJPFailsKarnataka #ಕರುನಾಡದ್ರೋಹಿಬಿಜೆಪಿ #KarnatakaAssemblyElections",
-"In Karnataka, the BJP has denied people's participation in governance by delaying BBMP, Zilla Panchayat, Taluk Elections. #VoteWisely. #BJPFailsKarnataka #ಕರುನಾಡದ್ರೋಹಿಬಿಜೆಪಿ #KarnatakaAssemblyElections ",
-"BJP violated the people's mandate, and came to power by bringing down a democratically formed government & undermining democracy through Operation Kamala. #VoteWisely. #BJPFailsKarnataka #ಕರುನಾಡದ್ರೋಹಿಬಿಜೆಪಿ #KarnatakaAssemblyElections",
-"Instead of improving diets and consumption of iron rich foods, our communal government is bringing in cattle slaughter bans and religion/caste into mid day meal schemes. #VoteWisely #BJPFailsKarnataka #ಕರುನಾಡದ್ರೋಹಿಬಿಜೆಪಿ #KarnatakaAssemblyElections",
-"The Karnataka government is trying to divert scholarship money for SC/ST children to itself. This affects the constitutional rights of ST/SC childrens access to education. #VoteWisely #BJPFailsKarnataka #ಕರುನಾಡದ್ರೋಹಿಬಿಜೆಪಿ #KarnatakaAssemblyElections",
-"In Karnataka today, there is no equality before law. Actor Chetan Ahimsa was arrested for a tweet whereas , Sharan Pumpwell who justified killing of Muslims, and many more have never been arrested #VoteWisely #BJPFailsKarnataka #ಕರುನಾಡದ್ರೋಹಿಬಿಜೆಪಿ #KarnatakaAssemblyElections", 
-"Under BJP rule in Karnataka today, Politicians who conduct protests do not face FIRs but farmers, workers and citizens do #VoteWisely #BJPFailsKarnataka #ಕರುನಾಡದ್ರೋಹಿಬಿಜೆಪಿ #KarnatakaAssemblyElections", 
-"Despite the Dec 31st, 2022 deadline set by the High Court, the Karnataka government has not held BBMP elections, leading to a weakening of key institutions. #VoteWisely #BJPFailsKarnataka #ಕರುನಾಡದ್ರೋಹಿಬಿಜೆಪಿ #KarnatakaAssemblyElections",
-"Under the BJP government the decades old State Planning Board was dissolved & the State Institute for Transforming Karnataka (SITK) set up. The SITK is largely inactive & key policies haven't been formulated #VoteWisely #BJPFailsKarnataka #ಕರುನಾಡದ್ರೋಹಿಬಿಜೆಪಿ",
-"Several hostels run by the Dept of Social Welfare are underfunded & mismanaged by the current govt. Protests against poor food & facilities in hostels have led to the rustication of students in Bengaluru, Mysuru & Bellari. #VoteWisely #BJPFailsKarnataka #KarnatakaAssemblyElections", 
-"Poor food and facilities in the hostels run by the Dept of Social Welfare have led to student protests in Bengaluru, Mysuru, and Bellari. The Karnataka govt has not addressed these issues. #VoteWisely #BJPFailsKarnataka #ಕರುನಾಡದ್ರೋಹಿಬಿಜೆಪಿ #KarnatakaAssemblyElections",
-"Instead of developing Bangalore University, the Karnataka govt is wasting funds on building a statue of a deity on campus. This inappropriate fund utilization shows the govt's priorities are misplaced. #VoteWisely #BJPFailsKarnataka #KarnatakaAssemblyElections",
+  "Karnataka has highest teacher vacancy in India. #VoteWisely #BJPFailsKarnataka #ಕರುನಾಡದ್ರೋಹಿಬಿಜೆಪಿ #KarnatakaAssemblyElections",
+  "In Karnataka, the BJP has denied people's participation in governance by delaying BBMP, Zilla Panchayat, Taluk Elections. #VoteWisely. #BJPFailsKarnataka #ಕರುನಾಡದ್ರೋಹಿಬಿಜೆಪಿ #KarnatakaAssemblyElections ",
+  "BJP violated the people's mandate, and came to power by bringing down a democratically formed government & undermining democracy through Operation Kamala. #VoteWisely. #BJPFailsKarnataka #ಕರುನಾಡದ್ರೋಹಿಬಿಜೆಪಿ #KarnatakaAssemblyElections",
+  "Instead of improving diets and consumption of iron rich foods, our communal government is bringing in cattle slaughter bans and religion/caste into mid day meal schemes. #VoteWisely #BJPFailsKarnataka #ಕರುನಾಡದ್ರೋಹಿಬಿಜೆಪಿ #KarnatakaAssemblyElections",
+  "The Karnataka government is trying to divert scholarship money for SC/ST children to itself. This affects the constitutional rights of ST/SC childrens access to education. #VoteWisely #BJPFailsKarnataka #ಕರುನಾಡದ್ರೋಹಿಬಿಜೆಪಿ #KarnatakaAssemblyElections",
+  "In Karnataka today, there is no equality before law. Actor Chetan Ahimsa was arrested for a tweet whereas , Sharan Pumpwell who justified killing of Muslims, and many more have never been arrested #VoteWisely #BJPFailsKarnataka #ಕರುನಾಡದ್ರೋಹಿಬಿಜೆಪಿ #KarnatakaAssemblyElections", 
+  "Under BJP rule in Karnataka today, Politicians who conduct protests do not face FIRs but farmers, workers and citizens do #VoteWisely #BJPFailsKarnataka #ಕರುನಾಡದ್ರೋಹಿಬಿಜೆಪಿ #KarnatakaAssemblyElections", 
+  "Despite the Dec 31st, 2022 deadline set by the High Court, the Karnataka government has not held BBMP elections, leading to a weakening of key institutions. #VoteWisely #BJPFailsKarnataka #ಕರುನಾಡದ್ರೋಹಿಬಿಜೆಪಿ #KarnatakaAssemblyElections",
+  "Under the BJP government the decades old State Planning Board was dissolved & the State Institute for Transforming Karnataka (SITK) set up. The SITK is largely inactive & key policies haven't been formulated #VoteWisely #BJPFailsKarnataka #ಕರುನಾಡದ್ರೋಹಿಬಿಜೆಪಿ",
+  "Several hostels run by the Dept of Social Welfare are underfunded & mismanaged by the current govt. Protests against poor food & facilities in hostels have led to the rustication of students in Bengaluru, Mysuru & Bellari. #VoteWisely #BJPFailsKarnataka #KarnatakaAssemblyElections", 
+  "Poor food and facilities in the hostels run by the Dept of Social Welfare have led to student protests in Bengaluru, Mysuru, and Bellari. The Karnataka govt has not addressed these issues. #VoteWisely #BJPFailsKarnataka #ಕರುನಾಡದ್ರೋಹಿಬಿಜೆಪಿ #KarnatakaAssemblyElections",
+  "Instead of developing Bangalore University, the Karnataka govt is wasting funds on building a statue of a deity on campus. This inappropriate fund utilization shows the govt's priorities are misplaced. #VoteWisely #BJPFailsKarnataka #KarnatakaAssemblyElections",
   "The politicization of education in Karnataka is a serious issue that needs to be addressed. Local MLAs should not be appointed to School Development and Monitoring Committees. #VoteWisely #BJPFailsKarnataka #ಕರುನಾಡದ್ರೋಹಿಬಿಜೆಪಿ #KarnatakaAssemblyElections", 
   "Under the current govt, Karnataka’s ability to be accountable to its own people, rather than the whims of dictatorial leaders sitting in New Delhi, has never before been so fundamentally threatened #BJPFailsKarnataka  #VoteWisely #ಕರುನಾಡದ್ರೋಹಿಬಿಜೆಪಿ #KarnatakaAssemblyElections",
   "Under the current govt, The State’s share of central taxes ; GST & funds for Centrally Sponsored Schemes has dropped significantly. #BJPFailsKarnataka  #VoteWisely #ಕರುನಾಡದ್ರೋಹಿಬಿಜೆಪಿ #KarnatakaAssemblyElections",
   "The Karnataka State Government made no attempt to resist the takeover of the diary sector by Amul & multi-state cooperative societies. This will impact 25 lakh farmer families dependent on Nandini #BJPFailsKarnataka #VoteWisely #ಕರುನಾಡದ್ರೋಹಿಬಿಜೆಪಿ #KarnatakaAssemblyElections",
   "The lack of transparency in the tenders called for by this  government has been highlighted by an MLA of the ruling party itself. #VoteWisely #BJPFailsKarnataka #ಕರುನಾಡದ್ರೋಹಿಬಿಜೆಪಿ #KarnatakaAssemblyElections",
   "Under the current ruling party of Karnataka, during the process of recruitment for 545 PSI posts held in October 2021, illegality was alleged as bribes amounting to crores were taken from candidates. #VoteWisely #BJPFailsKarnataka #ಕರುನಾಡದ್ರೋಹಿಬಿಜೆಪಿ #KarnatakaAssemblyElections", 
-
-
+  "Under the current ruling party of Karnataka, the Chilume scandal showed how BBMP officials worked to illegally collect voter data and revise rolls, compromising free & fair elections. #VoteWisely #BJPFailsKarnataka #ಕರುನಾಡದ್ರೋಹಿಬಿಜೆಪಿ #KarnatakaAssemblyElections",
+  "The deletion of voters of Muslim, Dalit and Christian votes shows that Karnataka's BJP government is not committed to free and fair elections #VoteWisely #BJPFailsKarnataka #ಕರುನಾಡದ್ರೋಹಿಬಿಜೆಪಿ #KarnatakaAssemblyElections",
+  "In Karnataka today, there is no equality before law. Actor Chetan Ahimsa was arrested for a tweet whereas Kali Swamy who called for killing of Muslims and many more have never been arrested #VoteWisely #BJPFailsKarnataka #ಕರುನಾಡದ್ರೋಹಿಬಿಜೆಪಿ #KarnatakaAssemblyElections",
+  "ನಂದಿನಿಯನ್ನು ಮಾರುವ ಸರ್ಕಾರ ನಮಗೆ ಬೇಡ ! #BJPFailsKarnataka #ಕರುನಾಡದ್ರೋಹಿಬಿಜೆಪಿ #KarnatakaAssemblyElection",
+  "ರೈತರಿಗೆ ಬೇಡವಾದ ಕೃಷಿ ಕಾನೂನು ತಂದ ಸರ್ಕಾರಕ್ಕೆ ದಿಕ್ಕಾರ! #BJPFailsKarnataka #ಕರುನಾಡದ್ರೋಹಿಬಿಜೆಪಿ #KarnatakaAssemblyElection",
+  "Fact finding reports by civil society members have shown how 100s of Muslims were arrested & charged under UAPA for the DJ Halli violence irrespective of whether they were present at the venue or not.#VoteWisely #BJPFailsKarnataka #ಕರುನಾಡದ್ರೋಹಿಬಿಜೆಪಿ #KarnatakaAssemblyElections",
+  "Education should be free from political interference, but BJP govt in Karnataka has let local MLAs take over School Development and Monitoring Committees, violating the right to education act. #VoteWisely #BJPFailsKarnataka #ಕರುನಾಡದ್ರೋಹಿಬಿಜೆಪಿ #KarnatakaAssemblyElections", 
+  "By removing Chairpersons of School Development and Monitoring Committees, the Karnataka govt is violating the Right to Education Act and encouraging misuse of school funds. #VoteWisely #BJPFailsKarnataka #ಕರುನಾಡದ್ರೋಹಿಬಿಜೆಪಿ #KarnatakaAssemblyElections",
+  "Nalin Kumar Kateel, President, BJP (Karnataka): ‘All followers of Tipu Sultan should be killed. Tipu Sultan’s family should be chased into the forests’ (reported February 15, 2023).... hate speech yaake? #BJPFailsKarnataka  #VoteWisely #ಕರುನಾಡದ್ರೋಹಿಬಿಜೆಪಿ #KarnatakaAssemblyElections",
+  "Inspite of warnings by climate experts, the state govt. has not been adequately preparing to combat climate change or build in resilience. #BJPFailsKarnataka #VoteWisely #ಕರುನಾಡದ್ರೋಹಿಬಿಜೆಪಿ #KarnatakaAssemblyElections",
+  "The blatantly unconstitutional Karnataka Protection of Right to Freedom of Religion Act (known as the Anticonversion law), passed in September 2022 further legitimizes vigilantism and moral policing. #BJPFailsKarnataka #VoteWisely #ಕರುನಾಡದ್ರೋಹಿಬಿಜೆಪಿ #KarnatakaAssemblyElections",
+  "The current Karnataka govt has made no progress on Climate Change commitments, ignored Environmental Safeguards and has  left Pollution Largely Unchecked #BJPFailsKarnataka #VoteWisely #ಕರುನಾಡದ್ರೋಹಿಬಿಜೆಪಿ #KarnatakaAssemblyElections",
+  "This government has failed to curb undemocratic activities by anti-social elements which affected harmony. #VoteWisely #BJPFailsKarnataka #ಕರುನಾಡದ್ರೋಹಿಬಿಜೆಪಿ #KarnatakaAssemblyElections",
+  "This govt has failed to curb calls for economic boycott of Muslim vendors, harassing of Muslim girl students, and attack on Christians. This has affected the harmony in Karnataka. #VoteWisely #BJPFailsKarnataka #ಕರುನಾಡದ್ರೋಹಿಬಿಜೆಪಿ #KarnatakaAssemblyElections"
+  
 
 
 ]
